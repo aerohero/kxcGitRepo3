9346a14256beecf66e2ff1bbe455259696652354
aerohero <aerohero@icloud.com>
2025-04-03 20:17:31 +0900

    Step 1
    project 생성

---
+++ HelloGit3/HelloGit3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloGit3
+//
+//  Created by Sean on 4/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
